6a6af563b4d080aa5fc6f55aae8064f3db8bad06
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-14 16:00:33 +0800

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,81 @@
+var useWebSockets = (function (react) {
+  'use strict';
+
+  var useWebSocketsOnOpen = (ws) => {
+    const [state, setState] = react.useState(null);
+
+    react.useEffect(() => {
+      const listener = (event) => {
+        setState(event);
+      };
+      ws.addEventListener('open', listener);
+
+      return () => {
+        ws.removeEventListener('open', listener);
+      };
+    });
+
+    return state;
+  };
+
+  var useWebSocketsOnClose = (ws) => {
+    const [state, setState] = react.useState(null);
+
+    react.useEffect(() => {
+      const listener = (event) => {
+        setState(event);
+      };
+      ws.addEventListener('close', listener);
+
+      return () => {
+        ws.removeEventListener('close', listener);
+      };
+    });
+
+    return state;
+  };
+
+  var useWebSocketsOnError = (ws) => {
+    const [state, setState] = react.useState(null);
+
+    react.useEffect(() => {
+      const listener = (event) => {
+        setState(event);
+      };
+      ws.addEventListener('error', listener);
+
+      return () => {
+        ws.removeEventListener('error', listener);
+      };
+    });
+
+    return state;
+  };
+
+  var useWebSocketsOnMessage = (ws) => {
+    const [state, setState] = react.useState(null);
+
+    react.useEffect(() => {
+      const listener = (event) => {
+        setState(event);
+      };
+      ws.addEventListener('message', listener);
+
+      return () => {
+        ws.removeEventListener('message', listener);
+      };
+    });
+
+    return state;
+  };
+
+  var index = {
+    onOpen: useWebSocketsOnOpen,
+    onClose: useWebSocketsOnClose,
+    onError: useWebSocketsOnError,
+    onMessage: useWebSocketsOnMessage,
+  };
+
+  return index;
+
+}(React));
